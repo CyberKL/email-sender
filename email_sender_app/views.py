a7cac1908828bb0c415eb92ee78f6cc3c4cc57a5
CyberKL <lotfyk29@yahoo.com>
2024-05-31 16:54:11 +0300

    Finish Assignment

--- email_sender_app/views.py
+++ email_sender_app/views.py
@@ -8,12 +8,13 @@ from form_submmission import fill_form
 def send_email(request):
     form_link = 'https://forms.gle/WT68aV5UnPajeoSc8'
     fill_form(form_link)
+    
     email = EmailMessage(
         'Python (Selenium) Assignment - Khaled Lotfy',
         'Please find here the attached screenshot of the confirmation page.',
         'klotfy785@gmail.com',
-        ['lotfyk29@yahoo.com'],
-
+        ['tech@themedius.ai'],
+        cc=['HR@themedius.ai']
     )
     email.attach_file('confirmation.png')
     email.send()
--- form_submmission.py
+++ form_submmission.py
@@ -8,24 +8,27 @@ def fill_form(form_link):
 
     time.sleep(5)
     
-    # Finding inputs
+    # Find each input form
     items = driver.find_elements(By.XPATH, "//div[contains(@role, 'listitem')]")
     
     for item in items:
+
+        # get the input field (or fields in case of date of birth) and the title of this input
         inputs = item.find_elements(By.XPATH, ".//input[not(@type='hidden')] | .//textarea")
         title = item.find_element(By.XPATH, ".//span[contains(@class, 'M7eMe')]").text
+
         for input in inputs:
             role = input.get_attribute('role')
-            if role and role == 'combobox':
+            if role and role == 'combobox': # check if it is date of birth input
                 input.send_keys(10)
-            elif 'code' in title:
-                code = title.split(': ')[1]
+            elif 'code' in title: # check if the input requires entering a code
+                code = title.split(': ')[1] # extracting the code from the title
                 input.send_keys(code)
-            else:
+            else: # other inputs
                 input.send_keys('test')
 
-    submit_btn = driver.find_element(By.XPATH, "//div[contains(@role, 'button')]")
+    submit_btn = driver.find_element(By.XPATH, "//div[contains(@role, 'button')]") # get the submit button
     submit_btn.click()
 
     time.sleep(5)
-    driver.save_screenshot('confirmation.png')
+    driver.save_screenshot('confirmation.png') # take screenshot
